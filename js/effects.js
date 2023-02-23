8e3aed9487b366de8fdabdc885f57df5eb9221cb
José María <josemariagl99@gmail.com>
2023-02-23 20:04:42 +0100

    flecha de navegación

--- js/effects.js
+++ js/effects.js
@@ -5,4 +5,25 @@ $(document).ready(function () {
             heightStyle: "content"
         });
     });
+
+    // Botón volver arriba script
+    const btn_scrolltop = document.getElementById('btn_scrolltop');
+    btn_scrolltop.addEventListener('click', () => {
+        window.scrollTo(0,0);
+    });
+
+    window.onscroll = () => {
+      add_btn_scrolltop();
+    }
+
+    const add_btn_scrolltop = () => {
+      if (window.scrollY < 300) {
+        btn_scrolltop.classList.remove("btn-scrolltop-on");
+      } else {
+        btn_scrolltop.classList.add("btn-scrolltop-on");
+      }
+    };
+
+    // jQueryValidate
+    $("#commentForm").validate();
 });
