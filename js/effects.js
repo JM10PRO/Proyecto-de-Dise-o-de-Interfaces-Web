f567e6dbe5c7d6aaa9bc47b458d4c7b39b3ad3a7
José María <josemariagl99@gmail.com>
2023-02-24 15:30:09 +0100

    botón ir abajo, formulacio con jqValidation Plugin

--- js/effects.js
+++ js/effects.js
@@ -6,15 +6,18 @@ $(document).ready(function () {
         });
     });
 
+    window.onscroll = () => {
+      // console.log(scrollY);
+      add_btn_scrolltop();
+      add_btn_scrolldown();
+    }
+
     // Botón volver arriba script
     const btn_scrolltop = document.getElementById('btn_scrolltop');
+    
     btn_scrolltop.addEventListener('click', () => {
         window.scrollTo(0,0);
     });
-
-    window.onscroll = () => {
-      add_btn_scrolltop();
-    }
 
     const add_btn_scrolltop = () => {
       if (window.scrollY < 300) {
@@ -24,6 +27,76 @@ $(document).ready(function () {
       }
     };
 
-    // jQueryValidate
-    $("#commentForm").validate();
+    // Botón ir abajo
+    const btn_scrolldown = document.getElementById('btn_scrolldown');
+    
+    btn_scrolldown.addEventListener('click', () => {
+      window.scrollTo(0,5000);
+    });
+
+    const add_btn_scrolldown = () => {
+      if (window.scrollY < 300) {
+        btn_scrolldown.classList.add("btn-scrolldown-on");
+      } else {
+        btn_scrolldown.classList.remove("btn-scrolldown-on");
+      }
+    };
+
+    // Formulario
+    var formularioContacto = $('#formulario');
+   
+    if(formularioContacto.length){
+      // jQueryValidate
+      formularioContacto.validate({
+        rules: {
+          nombre: {
+            required: true
+          },
+          apellidos: {
+            required: true
+          },
+          email: {
+            required: true,
+            email: true
+          },
+          entrada: {
+            required: true
+          },
+          asistente: {
+            required: true
+          },
+          grada: {
+            required: true
+          },
+        },
+        messages: {
+          nombre: {
+            required: 'El nombre es obligatorio'
+          },
+          apellidos: {
+            required: 'Por favor, escribe tus apellidos'
+          },
+          email: {
+            required: 'El email es obligatorio',
+            email: 'Por favor, introduce un email válido'
+          },
+          entrada: {
+            required: 'Por favor, elige el número de entradas'
+          },
+          asistente: 'Por favor, indica el tipo de asistente',
+          grada: {
+            required: 'Selecciona una opción'
+          },
+        },
+        errorPlacement: function(error, element) {
+          if(element.is(":radio")){
+            error.appendTo(element.parents('.radio'));
+          }else if(element.is(":checkbox")){
+            error.appendTo(element.parents('.checkbox'));
+          } else {
+            error.insertAfter(element);
+          }
+        }
+      });
+    }
 });
